01cd657f67e9ddff3da7e05c6eadd4f5bf3e7ee7
Daoyuan Wang <daoyuan.wang@intel.com>
2017-05-27 12:15:23 +0800

    leave some test suite from spark untouched (#224)
    * remove some suite could cause test fail in standalone package
    
    * fix CodecFactorySuite
    
    * fix style
    
    * set convert parquet flag

--- sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveParquetSuite.scala
+++ sql/hive/src/test/scala/org/apache/spark/sql/hive/HiveParquetSuite.scala
@@ -20,7 +20,6 @@ package org.apache.spark.sql.hive
 import org.apache.spark.sql.{QueryTest, Row}
 import org.apache.spark.sql.execution.datasources.parquet.ParquetTest
 import org.apache.spark.sql.hive.test.TestHiveSingleton
-import org.apache.spark.util.Utils
 
 case class Cases(lower: String, UPPER: String)
 
