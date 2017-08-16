0b78db5d94999812d35b4dfc1771895cbf0a4d3c
Li Lei <lee.lei.0309@gmail.com>
2017-08-16 16:42:56 +0800

    Add the compression type for the oap data file name if it's compresed. (#314)
    * Add the compression type for the oap data file name
    
    If the oap is uncompressed, keep the original file name.
    Verified that the data file name is changed correctly as the
    compression type is changed dynamically.
    
    * Nothing to do if the oap data file is uncompressed.
    
    If the oap data file is uncompressed, keep the original file name.
    
    * Avoid to use var
    
    No need var in order to add the compression type to the data file name
    
    * Be more concise with the functional programming style
    
    * trim the whitespaces if any
    
    * Add one test case to test if the compression type is added as expected
    
    * Keep concise with scala style
    
    * Use foreach to test each compression types
    
    * Following the scala style to be more concise

--- src/main/scala/org/apache/spark/sql/execution/datasources/oap/OapFileFormat.scala
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/OapFileFormat.scala
@@ -343,11 +343,24 @@ private[oap] class OapOutputWriter(
     partitionString = ps
   }
   private val writer: OapDataWriter = {
-    val isCompressed: Boolean = FileOutputFormat.getCompressOutput(context)
+    val isCompressed = FileOutputFormat.getCompressOutput(context)
+    val conf = context.getConfiguration()
+    val compressionType =
+      conf.get(OapFileFormat.COMPRESSION, OapFileFormat.DEFAULT_COMPRESSION).trim()
+    val compressionFileFormat =
+      if (!compressionType.isEmpty() && !compressionType.matches("UNCOMPRESSED")) {
+        "." + compressionType.toLowerCase()
+      } else ""
     val file: Path = new Path(path)
-    val fs: FileSystem = file.getFileSystem(context.getConfiguration)
-    val fileOut: FSDataOutputStream = fs.create(file, false)
-    new OapDataWriter(isCompressed, fileOut, dataSchema, context.getConfiguration)
+    // TODO: use extension in data file name
+    /*
+    val file =
+      new Path(path, getFileName(compressionFileFormat + OapFileFormat.OAP_DATA_EXTENSION))
+    */
+    val fs = file.getFileSystem(conf)
+    val fileOut = fs.create(file, false)
+
+    new OapDataWriter(isCompressed, fileOut, dataSchema, conf)
   }
 
   override def write(row: Row): Unit = throw new NotImplementedError("write(row: Row)")
