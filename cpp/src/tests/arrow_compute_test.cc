da32cc85522cd3f22c1cc5856cf8da7420c14e1d
Chendi Xue <chendi.xue@intel.com>
2020-02-14 10:38:17 +0800

    [CPP] Multiple Key Groupby fix and optimization
    Noticed before groupby with multiple key returns incorrect result, and this commit will fix this
    Also if multiple keys are all string, I will concat them with gandiva and do a hash firstly then doing encodeArray.
    By doing which, will be a little faster then directly hash and add
    
    Signed-off-by: Chendi Xue <chendi.xue@intel.com>

--- cpp/src/tests/arrow_compute_test.cc
+++ cpp/src/tests/arrow_compute_test.cc
@@ -545,12 +545,6 @@ TEST(TestArrowCompute, GroupByTwoAggregateWithMultipleBatchTest) {
 
   ////////////////////// calculation /////////////////////
   std::vector<std::string> input_data = {
-      /*"[\"a\", \"b\", \"c\", \"d\", \"e\", \"e\", \"d\", \"a\", \"b\", \"b\", \"a\", "
-      "\"a\", \"a\", \"d\", \"d\", \"c\", "
-      "\"e\", \"e\", \"e\", \"e\"]",
-      "[\"a\", \"b\", \"c\", \"d\", \"e\", \"e\", \"d\", \"a\", \"b\", \"b\", \"a\", "
-      "\"a\", \"a\", \"d\", \"d\", \"c\", "
-      "\"e\", \"e\", \"e\", \"e\"]",*/
       "[1, 2, 3, 4, 5, 5, 4, 1, 2, 2, 1, 1, 1, 4, 4, 3, 5, 5, 5, 5]",
       "[1, 2, 3, 4, 5, 5, 4, 1, 2, 2, 1, 1, 1, 4, 4, 3, 5, 5, 5, 5]",
       "[1, 2, 3, 4, 5, 5, 4, 1, 2, 2, 1, 1, 1, 4, 4, 3, 5, 5, 5, 5]"};
@@ -558,10 +552,6 @@ TEST(TestArrowCompute, GroupByTwoAggregateWithMultipleBatchTest) {
   ASSERT_NOT_OK(expr->evaluate(input_batch, &output_batch_list));
 
   std::vector<std::string> input_data_2 = {
-      /*"[\"f\", \"g\", \"h\", \"i\", \"j\", \"j\", \"i\", \"g\", \"h\", \"i\", "
-      "\"g\",\"g\", \"g\", \"j\", \"i\", \"f\", \"f\", \"i\", \"j\", \"i\"]",
-      "[\"f\", \"g\", \"h\", \"i\", \"j\", \"j\", \"i\", \"g\", \"h\", \"i\", "
-      "\"g\",\"g\", \"g\", \"j\", \"i\", \"f\", \"f\", \"i\", \"j\", \"i\"]",*/
       "[6, 7, 8, 9, 10, 10, 9, 6, 7, 7, 6, 6, 6, 9, 9, 8, 10, 10, 10, 10]",
       "[6, 7, 8, 9, 10, 10, 9, 6, 7, 7, 6, 6, 6, 9, 9, 8, 10, 10, 10, 10]",
       "[6, 7, 8, 9, 10, 10, 9, 6, 7, 7, 6, 6, 6, 9, 9, 8, 10, 10, 10, 10]"};
@@ -585,6 +575,76 @@ TEST(TestArrowCompute, GroupByTwoAggregateWithMultipleBatchTest) {
       "[8, 10, 9, 20, 55, 42, 28, 32, 54, 70]"};
   auto res_sch = arrow::schema({f_unique_0, f_unique_1, f_sum});
   MakeInputBatch(expected_result_string, res_sch, &expected_result);
+  ASSERT_NOT_OK(Equals(*expected_result.get(), *(result_batch[0]).get()));
+}
+
+TEST(TestArrowCompute, GroupByTwoUtf8AggregateWithMultipleBatchTest) {
+  ////////////////////// prepare expr_vector ///////////////////////
+  auto f0 = field("f0", utf8());
+  auto f1 = field("f1", utf8());
+  auto f2 = field("f2", uint32());
+  auto f_unique_0 = field("unique", utf8());
+  auto f_unique_1 = field("unique", utf8());
+  auto f_sum = field("sum", uint64());
+  auto f_res = field("res", uint64());
+
+  auto arg0 = TreeExprBuilder::MakeField(f0);
+  auto arg1 = TreeExprBuilder::MakeField(f1);
+  auto arg2 = TreeExprBuilder::MakeField(f2);
+  auto n_pre = TreeExprBuilder::MakeFunction("encodeArray", {arg0, arg1}, uint32());
+
+  auto n_split = TreeExprBuilder::MakeFunction("splitArrayListWithAction",
+                                               {n_pre, arg0, arg1, arg2}, uint32());
+  auto arg_res = TreeExprBuilder::MakeField(f_res);
+  auto n_unique_0 =
+      TreeExprBuilder::MakeFunction("action_unique", {n_split, arg0}, uint32());
+  auto n_unique_1 =
+      TreeExprBuilder::MakeFunction("action_unique", {n_split, arg1}, uint32());
+  auto n_sum = TreeExprBuilder::MakeFunction("action_sum", {n_split, arg2}, uint32());
+
+  auto unique_expr_0 = TreeExprBuilder::MakeExpression(n_unique_0, f_res);
+  auto unique_expr_1 = TreeExprBuilder::MakeExpression(n_unique_1, f_res);
+  auto sum_expr = TreeExprBuilder::MakeExpression(n_sum, f_res);
+
+  std::vector<std::shared_ptr<::gandiva::Expression>> expr_vector = {
+      unique_expr_0, unique_expr_1, sum_expr};
+  auto sch = arrow::schema({f0, f1, f2});
+  std::vector<std::shared_ptr<Field>> ret_types = {f_unique_0, f_unique_1, f_sum};
+
+  /////////////////////// Create Expression Evaluator ////////////////////
+  std::shared_ptr<CodeGenerator> expr;
+  ASSERT_NOT_OK(CreateCodeGenerator(sch, expr_vector, ret_types, &expr, true));
+  std::shared_ptr<arrow::RecordBatch> input_batch;
+  std::vector<std::shared_ptr<arrow::RecordBatch>> output_batch_list;
+
+  ////////////////////// calculation /////////////////////
+  std::vector<std::string> input_data = {
+      R"(["a", "b", "c", "d", "e", "e", "d", "a", "b", "b", "a", "a", "a", "d", "d", "c", "e", "e", "e", "e"])",
+      R"(["BJ", "SH", "SZ", "HZ", "WH", "WH", "HZ", "BJ", "SH", "SH", "BJ", "BJ", "BJ", "HZ", "HZ", "SZ", "WH", "WH", "WH", "WH"])",
+      R"([1, 2, 3, 4, 5, 5, 4, 1, 2, 2, 1, 1, 1, 4, 4, 3, 5, 5, 5, 5])"};
+  MakeInputBatch(input_data, sch, &input_batch);
+  ASSERT_NOT_OK(expr->evaluate(input_batch, &output_batch_list));
+
+  std::vector<std::string> input_data_2 = {
+      R"(["f", "g", "h", "i", "j", "j", "i", "g", "h", "i", "g", "g", "g", "j", "i", "f", "f", "i", "j", "j"])",
+      R"(["CD", "DL", "NY", "LA", "AU", "AU", "LA", "DL", "NY", "LA", "DL", "DL", "DL", "AU", "LA", "CD", "CD", "LA", "AU", "AU"])",
+      "[6, 7, 8, 9, 10, 10, 9, 6, 7, 7, 6, 6, 6, 9, 9, 8, 10, 10, 10, 10]"};
+  MakeInputBatch(input_data_2, sch, &input_batch);
+  ASSERT_NOT_OK(expr->evaluate(input_batch, &output_batch_list));
+
+  ////////////////////// Finish //////////////////////////
+  std::vector<std::shared_ptr<arrow::RecordBatch>> result_batch;
+  ASSERT_NOT_OK(expr->finish(&result_batch));
+
+  std::shared_ptr<arrow::RecordBatch> expected_result;
+  std::vector<std::string> expected_result_string = {
+      R"(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"])",
+      R"(["BJ", "SH", "SZ", "HZ", "WH", "CD", "DL", "NY", "LA", "AU"])",
+      "[5, 6, 6, 16, 30, 24, 31, 15, 44, 49]"};
+  auto res_sch = arrow::schema({f_unique_0, f_unique_1, f_sum});
+  MakeInputBatch(expected_result_string, res_sch, &expected_result);
+
+  std::cout << "3" << std::endl;
   ASSERT_NOT_OK(Equals(*expected_result.get(), *(result_batch[0]).get()));
 }
 
